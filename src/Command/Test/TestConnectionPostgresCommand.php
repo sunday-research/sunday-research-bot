a40591044b4c4d947cfcee46857c8069f70de9e0
Vladislav Subbotin <subb98@gmail.com>
2024-07-12 23:56:07 +0500

    Issue 13 (#21)
    * Add PostgreSQL container
    
    * Add symfony/orm-pack package
    
    * Add test commands

---
+++ src/Command/Test/TestConnectionPostgresCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command\Test;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Throwable;
+
+#[AsCommand(
+    name: 'test:connection-postgres',
+    description: 'Command for testing connection to PostgreSQL server',
+)]
+final class TestConnectionPostgresCommand extends Command
+{
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        try {
+            $connect = $this->entityManager->getConnection();
+            $result = $connect->executeQuery('SELECT NOW()');
+            $io->success("PostgreSQL is running, current time: {$result->fetchOne()}");
+            $connect->close();
+            return Command::SUCCESS;
+        } catch (Throwable $e) {
+            $io->error("Something went wrong.. Error message: {$e->getMessage()}");
+            return Command::FAILURE;
+        }
+    }
+}
